ca97c9c8e067477597136ba34a607ad93427d1cb
Zoltan Szepesi <zoltan.szepesi@ibm.com>
2022-12-13 15:38:24 +0100

    Basic scaffold

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EmptyComponent} from "./layouts/empty/empty.component";
+import {FullComponent} from "./layouts/full/full.component";
+import {RedirectToDashboardGuard} from "./guards/redirect-to-dashboard.guard";
+import {NotFoundComponent} from "./components/not-found/not-found.component";
+import {RedirectToLoginGuard} from "./guards/redirect-to-login.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EmptyComponent,
+    children: [
+      {
+        path: '',
+        canActivate: [RedirectToDashboardGuard],
+        loadChildren: () => import('./modules/auth/auth.module')
+      }
+    ]
+  },
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      {
+        path: 'admin',
+        canActivate: [RedirectToLoginGuard],
+        loadChildren: () => import('./modules/admin/admin.module')
+      }
+    ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {InputModule} from "carbon-components-angular";
+import { EmptyComponent } from './layouts/empty/empty.component';
+import { FullComponent } from './layouts/full/full.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmptyComponent,
+    FullComponent,
+    NotFoundComponent
   ],
     imports: [
         BrowserModule,
