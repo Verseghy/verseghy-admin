25cc95739694d13b039d64417d3ff055600060ab
Zoltan Szepesi <zoltan.szepesi@ibm.com>
2023-01-01 07:48:25 +0100

    Fix empty table

--- src/app/modules/admin/modules/users/components/list/list.component.ts
+++ src/app/modules/admin/modules/users/components/list/list.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { UserService } from '../../services/user.service'
 import {
   IconService,
   TableHeaderItem,
+  TableItem,
   TableModel,
+  TableRow,
 } from 'carbon-components-angular'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { TrashCan16 } from '@carbon/icons'
+import { map } from 'rxjs'
 
 const headers = [
   new TableHeaderItem({ data: $localize`ID` }),
@@ -20,15 +23,36 @@ const headers = [
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
 })
-export class ListComponent {
-  users$ = this.userService.getAll()
-  model = new TableModel()
+export class ListComponent implements OnInit {
+  emptyTableModel = new TableModel()
+  model$ = this.userService.entities$.pipe(
+    map((users) => {
+      const model = new TableModel()
+      model.header = headers
+      let data: TableRow[] = []
+      for (const user of users) {
+        data = [
+          ...data,
+          new TableRow(
+            new TableItem({ data: user.id }),
+            new TableItem({ data: user.name }),
+            new TableItem({ data: user.email })
+          ),
+        ]
+      }
+      model.data = data
+      return model
+    })
+  )
 
   constructor(
     private userService: UserService,
     private iconService: IconService
   ) {
     this.iconService.registerAll([TrashCan16])
-    this.model.header = headers
+  }
+
+  ngOnInit() {
+    this.userService.getAll()
   }
 }
