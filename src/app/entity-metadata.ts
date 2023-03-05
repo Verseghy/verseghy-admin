e8e661bc07fa7fb8ce33f725174db48b5f1021f7
Zoltán Szepesi <2dcube01@gmail.com>
2023-03-05 03:02:46 +0100

    filter fn

--- src/app/entity-metadata.ts
+++ src/app/entity-metadata.ts
@@ -1,11 +1,16 @@
 import { EntityMetadataMap, EntityDataModuleConfig } from '@ngrx/data'
+import { Problem } from './modules/admin/modules/math/modules/problems/models/problem'
 
 const entityMetadata: EntityMetadataMap = {
   User: {},
   Action: {},
   Group: {},
   App: {},
-  'Math-Problem': {},
+  'Math-Problem': {
+    filterFn: (entities: Problem[], pattern) => {
+      return entities.filter((value) => value.body.includes(pattern))
+    },
+  },
 }
 
 const pluralNames = {}
