64737aa8bae835d29460d622ac0413a02df2edfe
Zoltan Szepesi <2dcube01@gmail.com>
2024-12-08 06:05:21 +0100

    Bump 194

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   baseIAMURL: 'https://iam.194.verseghy-gimnazium.net',
-  baseMathcompetitionURL: 'https://iam.194.verseghy-gimnazium.net',
+  baseMathcompetitionURL: 'https://api.194.verseghy-gimnazium.net',
 }
